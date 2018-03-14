5393fb680321a745fc22f649e2a8b7e351ae6c88
guxiaojie <gujievivi@gmail.com>
2018-03-14 13:50:38 +1100

    Build Project; add Basic UI/Model

---
+++ CMPhotos/CMPhotos/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CMPhotos
+//
+//  Created by Guxiaojie on 14/03/2018.
+//  Copyright © 2018 SageGu. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ CMPhotos/CMPhotos/PhotoCollectionViewLayout.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewLayout.swift
+//  CMPhotos
+//
+//  Created by Guxiaojie on 14/03/2018.
+//  Copyright © 2018 SageGu. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewLayout: UICollectionViewLayout {
+
+}
